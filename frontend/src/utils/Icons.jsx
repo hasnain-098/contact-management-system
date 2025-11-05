ce265c74168e98f95275abccc47a7adbf65988dc
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-11-05 17:36:59 +0500

    fix: add missing import for PropTypes

--- frontend/src/utils/Icons.jsx
+++ frontend/src/utils/Icons.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'; 
 
 export const EyeIcon = (props) => (
     <svg {...props} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
