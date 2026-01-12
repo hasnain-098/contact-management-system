1a316b1a202762a4dae5c1e56b00c53fd89214d1
Hasnain Memon <hasnainmemon.018@gmail.com>
2026-01-12 17:48:58 +0500

    chore: load environment variables from .env

--- backend/src/main/java/com/hasnain/cms/CMSApplication.java
+++ backend/src/main/java/com/hasnain/cms/CMSApplication.java
@@ -1,5 +1,6 @@
 package com.hasnain.cms;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CMSApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        
 		SpringApplication.run(CMSApplication.class, args);
 	}
 
